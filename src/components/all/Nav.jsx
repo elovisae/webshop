f753c5e7de1194a848ef3e95f682e76b9e737980
Lovisa Eriksson <lovisa@ahna.se>
2022-03-21 08:49:55 +0100

    init nav

---
+++ src/components/all/Nav.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './nav.css'
+
+const Nav = () => {
+  return (
+    <nav>
+        <h1>AMANDA</h1>
+        <ul>
+            <li>Link</li>
+            <li>Link2</li>
+            <li>Link3</li>
+            <li>Link4</li>
+            <li>Link5</li>
+        </ul>
+        
+    </nav>
+  )
+}
+
+export default Nav
