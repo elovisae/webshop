ec9ae14cf7b1f69f18f0b0513a7e3477679ec36b
Lovisa Eriksson <lovisa@ahna.se>
2022-03-22 10:45:42 +0100

    added errorpage

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import HomePage from './components/homePage/HomePage';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import BestSellers from './components/bestsellers/BestSellers';
+import ErrorPage from './components/404';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage/>}/>
         <Route path="/bestsellers" element={<BestSellers/>}/>
+        <Route path="*" element={<ErrorPage/>}/>
       </Routes>
     </Router>
   );
